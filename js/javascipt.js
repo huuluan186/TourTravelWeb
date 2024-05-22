4914d2bcb3a14d0803b001a0e3dfe51a4bf17294
huuluan186 <luanphamhuu2004@gmail.com>
2024-05-22 12:12:49 +0700

    22/5

--- js/javascipt.js
+++ js/javascipt.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var ourQuality = document.getElementById('resize');
 
     function toggleContainerClass() {
-        if (window.innerWidth <= 800) {
+        if (window.innerWidth <= 370) {
             // hoặc bất kỳ kích thước breakpoint sm nào khác
             ourQuality.classList.remove('container');
             ourQuality.classList.add('container-fluid');
@@ -17,4 +17,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Gọi hàm toggleContainerClass khi cửa sổ trình duyệt được resize
     window.addEventListener('resize', toggleContainerClass);
+});
+
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+    var navLinks = document.querySelectorAll('.navbar-nav a'); // Chọn tất cả các thẻ a trong navbar-nav
+
+    if (window.scrollY > 50) {
+        // Đặt ngưỡng để thay đổi màu nền
+        navbar.style.backgroundColor = 'rgba(51, 51, 51, 0.7)'; // Màu nền mờ mờ
+        navLinks.forEach(function (link) {
+            link.style.color = '#fff'; // Màu chữ trắng cho tất cả các liên kết
+        });
+    } else {
+        navbar.style.backgroundColor = 'transparent'; // Màu nền ban đầu
+        navLinks.forEach(function (link) {
+            link.style.color = '#000'; // Màu chữ ban đầu cho tất cả các liên kết
+        });
+    }
 });
