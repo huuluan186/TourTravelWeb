f2a532e7cbf52b6b57b77b312985366320a8ef9a
huuluan186 <luanphamhuu2004@gmail.com>
2024-05-24 11:10:54 +0700

    24/5

--- js/javascipt.js
+++ js/javascipt.js
@@ -1,24 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var ourQuality = document.getElementById('resize');
-
-    function toggleContainerClass() {
-        if (window.innerWidth <= 370) {
-            // hoặc bất kỳ kích thước breakpoint sm nào khác
-            ourQuality.classList.remove('container');
-            ourQuality.classList.add('container-fluid');
-        } else {
-            ourQuality.classList.remove('container-fluid');
-            ourQuality.classList.add('container');
-        }
-    }
-
-    // Gọi hàm toggleContainerClass khi trang được tải lần đầu
-    toggleContainerClass();
-
-    // Gọi hàm toggleContainerClass khi cửa sổ trình duyệt được resize
-    window.addEventListener('resize', toggleContainerClass);
-});
-
 window.addEventListener('scroll', function () {
     var navbar = document.querySelector('.navbar');
     var navLinks = document.querySelectorAll('.navbar-nav a'); // Chọn tất cả các thẻ a trong navbar-nav
