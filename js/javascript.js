415be827ee118cca1ae929fa71befd1de8df6e68
huuluan186 <luanphamhuu2004@gmail.com>
2024-05-29 20:05:59 +0700

    thêm nội dung và sửa giá

--- js/javascript.js
+++ js/javascript.js
@@ -3,16 +3,9 @@ window.addEventListener('scroll', function () {
     var navLinks = document.querySelectorAll('.navbar-nav a'); // Chọn tất cả các thẻ a trong navbar-nav
 
     if (window.scrollY > 50) {
-        // Đặt ngưỡng để thay đổi màu nền
-        navbar.style.backgroundColor = 'rgba(51, 51, 51, 0.7)'; // Màu nền mờ mờ
-        navLinks.forEach(function (link) {
-            link.style.color = '#fff'; // Màu chữ trắng cho tất cả các liên kết
-        });
+        navbar.classList.add('scrolled');
     } else {
-        navbar.style.backgroundColor = 'transparent'; // Màu nền ban đầu
-        navLinks.forEach(function (link) {
-            link.style.color = '#000'; // Màu chữ ban đầu cho tất cả các liên kết
-        });
+        navbar.classList.remove('scrolled');
     }
 });
 
